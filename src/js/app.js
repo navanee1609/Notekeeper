b66944e1727f9777c56570157ba6aef4ad794de8
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-08 21:19:51 +0530

    added tooltip

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 
+// import 
 
+// import { Tooltip } from "./components/Tooltip";
 
 // toggle sidebar in small screen
 
@@ -92,5 +94,73 @@ if (currentdateElem) {
 } else {
     console.error("Element not found or does not exist.");
 }
+
+
+// adding tooltip
+
+
+// let tooltipElems = document.querySelectorAll('[data-tooltip]');
+
+// tooltipElems.forEach(elem=>Tooltip(elem))
+
+
+// const Tooltip= function (element){
+//     let tooltip=document.createElement('span')
+
+//     tooltip.classList.add('tooltip', 'text-body-small');
+
+//     element.addEventListener('mouseenter',function(){
+//         console.log(this.dataset.tooltip);
+//     })
+// }
+
+// Define the Tooltip function
+const Tooltip = function (element) {
+    // Create a tooltip element
+    let tooltip = document.createElement('span');
+    tooltip.classList.add('tooltip', 'text-body-small');
+
+    // Set the tooltip text from the 'data-tooltip' attribute
+    tooltip.textContent = element.dataset.tooltip;
+
+    // Add custom styling to the tooltip
+    tooltip.style.padding = '5px 10px';
+    tooltip.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+    tooltip.style.color = 'white'; 
+    tooltip.style.borderRadius = '5px'; 
+    tooltip.style.height = 'auto'; 
+
+    // Appending the tooltip element to the body
+    document.body.appendChild(tooltip);
+
+    // Position the tooltip relative to the element
+    element.addEventListener('mouseenter', function () {
+        // Get the position and dimensions of the element
+        let rect = element.getBoundingClientRect();
+
+        // Calculate the position for the tooltip
+        let top = rect.top - tooltip.offsetHeight - 10; 
+        let left = rect.left + (rect.width - tooltip.offsetWidth) / 2;
+
+        // Set the tooltip position
+        tooltip.style.top = top + 'px';
+        tooltip.style.left = left + 'px';
+
+        // Show the tooltip
+        tooltip.style.display = 'block';
+    });
+
+    // Hide the tooltip when mouse leaves the element
+    element.addEventListener('mouseleave', function () {
+        tooltip.style.display = 'none';
+    });
+};
+
+// Select elements with the 'data-tooltip' attribute
+let tooltipElems = document.querySelectorAll('[data-tooltip]');
+
+// Initialize tooltips for each matching element
+tooltipElems.forEach((elem) => Tooltip(elem));
+
 
 
--- src/js/components/Tooltip.js
+++ src/js/components/Tooltip.js
@@ -1,5 +1,14 @@
-/**
- * @copyright codewithsadee 2023
- */
 
-'use strict';
+
+// /*
+// @param {HTMLElement} element  =>dom el added to behaviour  */
+
+// export const Tooltip= function (element){
+//     let tooltip=document.createElement('span')
+
+//     tooltip.classList.add('tooltip', 'text-body-small');
+
+//     element.addEventListener('mouseenter',function{
+//         console.log(this.dataset.tooltip);
+//     })
+// }
