03a7767b5234573dc887b2c1ed01b953c13ad887
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-08 19:22:24 +0530

    declared and assigned sidebar toggler

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,13 @@
-/**
- * @copyright codewithsadee 2023
- */
+// Module import
 
-'use strict';
+
+
+
+// toggle sidebar in small screen
+
+
+let sidebar= document.querySelector('[data-sidebar]')
+
+let sidebarTogglers=document.querySelector('[data-sidebar-toggler]');
+
+let overlay=document.querySelector('[data-sidebar-overlay]')
