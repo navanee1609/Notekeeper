253c2dca718d3e791e379b551e5dc40de667f81c
Navaneethan KV <navaneethanvs18@gmail.com>
2023-10-08 20:15:26 +0530

    aded function to greet msg based on time

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,3 @@
-// Module import
-
-// import { addEventOnElements } from "./utilss";
-
-
-
-// import { addEventOnElements } from "./utils";
-
-
 
 
 
@@ -20,6 +11,13 @@ let sidebarTogglers=document.querySelectorAll('[data-sidebar-toggler]');
 let overlay=document.querySelector('[data-sidebar-overlay]')
 
 
+/**@param {Array<HTMLElements>} elements   ===>an array of DOM elements
+
+@param {string}  eventType ==>type of event to listen
+
+@param {Function} callback ==>function to be excuted when called (Event occurs)
+
+**/
 
 
 
@@ -35,3 +33,27 @@ addEventOnElements(sidebarTogglers, 'click', function(){
     sidebar.classList.toggle('active');
     overlay.classList.toggle('active')
 })
+
+
+// show greeting message on homepage
+
+
+
+let greetEle = document.querySelector('[data-greeting]');
+
+let currentHour = new Date().getHours()
+
+greetEle.textContent=getGreetingMsg()
+
+
+
+/* Generated a greeting based on current hour of the day
+
+@param {number} currentHour - (0-23)
+
+@returns {string} greeting with time of the day
+*/
+
+let getGreetingMsg =function (currentHour){
+
+}
--- src/js/theme.js
+++ src/js/theme.js
@@ -1,5 +1,35 @@
-/**
- * @copyright codewithsadee 2023
- */
+// initialising theme
 
-'use strict';
+
+let storedTheme = localStorage.getItem('theme');
+
+
+let systemThemeDark = window.matchMedia('(prefers-color-scheme:dark)').matches;
+
+let initialTheme= storedTheme ?? (systemThemeDark ? 'dark': 'light');
+
+document.documentElement.setAttribute('data-theme', initialTheme);
+
+
+// toggled b\w light and dark
+
+let toggleTheme= function(){
+    let currentTheme = document.documentElement.getAttribute('data-theme' || 'light');
+
+    let newTheme = currentTheme ===  'light' ? 'dark' : 'light';
+
+    document.documentElement.setAttribute('data-theme', newTheme);
+
+    localStorage.setItem('theme',newTheme)
+}
+
+
+
+
+// attaching toggler to icon button  via click event
+
+
+window.addEventListener('DOMContentLoaded', function(){
+    let themeBtn=this.document.querySelector('[data-theme-btn]');
+    if(themeBtn) themeBtn.addEventListener('click',toggleTheme)
+})
